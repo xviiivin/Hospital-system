5eeb639695ac1ae87e511a9169ef3bb95f99c10f
wiwat liangkobkit <wiwatwin1801@gmail.com>
2023-03-30 03:31:33 +0700

    admincheck

---
+++ client/src/views/admin/Alluser.vue
@@ -0,0 +1,29 @@
+<template>
+    <AppLayout>
+        <div class="w-full mx-auto">
+            <adminnav />
+
+
+
+        </div>
+    </AppLayout>
+</template>
+  
+<script>
+import AppLayout from "../../components/AppLayout.vue";
+import adminnav from '../../components/admin/Nav.vue'
+export default {
+    components: {
+        AppLayout,
+        adminnav
+    },
+    data() {
+        return {
+
+        };
+    },
+};
+</script>
+  
+<style></style>
+  
