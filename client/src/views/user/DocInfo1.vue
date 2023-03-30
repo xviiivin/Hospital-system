9f4386dac9351a4fa11441fa6e6e4494fe3cae11
Wiranyupaa <64070231@kmitl.ac.th>
2023-03-31 00:18:49 +0700

    alldoctor responsive

--- client/src/views/user/DocInfo1.vue
+++ client/src/views/user/DocInfo1.vue
@@ -12,28 +12,28 @@
 
                   <!-- body -->
                   <div class="relative h-full w-full md:w-2/3 xl:w-1/3 flex items-center rounded-t-3xl bg-white">
-                        <div class="w-full h-full p-7">
+                        <div class="w-full h-full p-8">
                               <div class="mt-3">
-                                    <p class="text-md text-[#B6B6B6] ml-8">ประวัติคุณหมอ</p>
+                                    <p class="text-md text-[#B6B6B6]">ประวัติคุณหมอ</p>
                                     <div v-for="(val, index) in DocInfo" :key="index">
-                                          <p class="text-lg font-bold mb-8 ml-8 underline">{{ val.docname }}</p>
-                                          <div class="">
-                                                <div class="col-start-2 col-end-7 md:col-start-3">
+                                          <p class="text-xl font-bold mb-8 underline">{{ val.docname }}</p>
+                                          <div class=" mb-3 ">
+                                                <div class="mx-5 mb-3 ">
                                                       <div
-                                                            class="rounded-xl w-[250px] h-[250px] group shadow-lg overflow-hidden ">
+                                                            class="rounded-xl w-full h-[calc(100%+2rem)] group shadow-lg overflow-hidden ">
                                                             <img class=" h-full w-full group-hover:scale-110 ease-in duration-300"
                                                                   :src="val.image">
                                                       </div>
                                                 </div>
-                                                <div class="col-start-1 col-end-7 mt-5">
-                                                      <p class="ml-8 font-bold text-lg">ประวัติ</p>
-                                                      <div class=" text-[#111727] ml-[40%]">
+                                                <div class="mt-5">
+                                                      <p class="ml-8 mb-3 font-bold text-lg underline">ประวัติ</p>
+                                                      <div class="text-[#111727] ml-[20%]">
                                                             <ul class="list-disc">
-                                                                  <li>{{ val.docname }}</li>
-                                                                  <li>{{ val.specialist }}</li>
-                                                                  <li>{{ val.age }}</li>
-                                                                  <li>{{ val.gender }}</li>
-                                                                  <li>{{ val.description }}</li>
+                                                                  <li>Name:  {{ val.docname }}</li>
+                                                                  <li>Expert:  {{ val.specialist }}</li>
+                                                                  <li>Age:  {{ val.age }}</li>
+                                                                  <li>Gender:  {{ val.gender }}</li>
+                                                                  <li>Description:  {{ val.description }}</li>
                                                             </ul>
                                                       </div>
                                                 </div>
