ab299e2962f4ba9837012984717dd8bdf0ba943b
wiwat liangkobkit <wiwatwin1801@gmail.com>
2023-04-03 22:43:28 +0700

    okay ptun bor hai push

--- client/src/components/Addinfo.vue
+++ client/src/components/Addinfo.vue
@@ -8,26 +8,26 @@
             </div>
             <div>
                 <div class="pb-4">
-                    <input class='w-full' type="text">
+                    <input class='w-full' type="text" v-model="Name">
                 </div>
                 <div class="pb-4">
-                    <input class='w-full' type="text">
+                    <input class='w-full' type="text" v-model="Age">
                 </div>
                 <div class="pb-4">
-                        <select id="underline_select"
-                        class="block py-2.5 px-2 w-full text-sm text-black bg-white  border-0 border-b-2   focus:outline-none focus:ring-0 focus:border-black peer">
-                        <option selected>Choose your gender</option>
+                    <select id="underline_select" v-model="Genders"
+                        class="py-2.5 px-2 w-full text-sm text-black bg-white  border-0 border-b-2  focus:outline-none focus:ring-0 focus:border-black ">
+                        <option value="" disabled>Choose your gender</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         <option value="Others">Others</option>
                     </select>
                 </div>
-                
+
                 <div class="pb-4">
-                    <input class='w-full' type="text">
+                    <input class='w-full' type="text" v-model="Expert">
                 </div>
                 <div>
-                    <textarea name="" class='w-full' id="" cols="30" rows="3"></textarea>
+                    <textarea name="" class='w-full' id="" cols="30" rows="3" v-model="Discription"></textarea>
                 </div>
             </div>
         </div>
@@ -39,7 +39,12 @@
 export default {
     data() {
         return {
-            left: ['Name', 'Age', 'Genders', 'Expert', 'Discription']
+            left: ['Name', 'Age', 'Genders', 'Expert', 'Discription'],
+            Name: '',
+            Age: '',
+            Genders: '',
+            Expert: '',
+            Discription: '',
         }
     },
 
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -11,20 +11,24 @@
         <div class="bg-gradient-to-r w-full from-[#111727] to-[#FF5757] h-1 rounded-full mt-2"></div>
         <p class="text-[#70737D] text-md mt-3">Login, W&N Hospital is ready to serve you.</p>
       </div>
-      <div class="flex flex-col w-2/3 xl:w-1/3 mt-10 gap-y-5">
-        <input v-model='name' type="text" class="border border-black rounded-full p-2 px-4 text-lg" placeholder="Name" />
-        <input v-model='idcard' type="text" class="border border-black rounded-full p-2 px-4 text-lg"
-          placeholder="Id-card" />
-        <input v-model='password' type="text" class="border border-black rounded-full p-2 px-4 text-lg"
-          placeholder="password" />
-        <input v-model='phone' type="text" class="border border-black rounded-full p-2 px-4 text-lg"
-          placeholder="phone number" />
-        <button class="w-full bg-primary text-white text-lg font-semibold py-2 rounded-md mt-10" @click="register()">
-          Register
-        </button>
-        <p class="text-sm xl:text-md text-gray-400 text-center">Have an account? <a @click="this.$router.push('/login')"
-            class="text-black/70 cursor-pointer">Back to login ✨</a></p>
-      </div>
+      <form @submit.prevent="register">
+        <div class="flex flex-col">
+          <input v-model='name' type="text" required class="border border-black rounded-full p-2 px-12 mt-4 mb-4 text-lg"
+            placeholder="Name" />
+          <input v-model='idcard' type="text" required class="border border-black rounded-full p-2 px-12 mb-4 text-lg"
+            placeholder="Id-card" />
+          <input v-model='password' type="text" required class="border border-black rounded-full p-2 px-12 mb-4 text-lg"
+            placeholder="password" />
+          <input v-model='phone' type="text" required class="border border-black rounded-full p-2 px-12 mb-2 text-lg"
+            placeholder="phone number" />
+          <button type="submit" class="w-full bg-primary text-white text-lg font-semibold py-4 rounded-md mb-4 mt-10">
+            Register
+          </button>
+          <p class="text-sm xl:text-md text-gray-400 text-center mb-8">Have an account? <a @click="this.$router.push('/login')"
+              class="text-black/70 cursor-pointer">Back to login ✨</a></p>
+        </div>
+      </form>
+
     </div>
   </AppLayout>
 </template>
@@ -69,8 +73,13 @@ export default {
       const data = {
         name: this.name, idCard: this.idcard, password: this.password, phone: this.phone
       }
-      console.log(data);
-      if (this.name !== '' && this.idCard !== '' && this.password !== '' && this.phone !== '') {
+      if ((this.idcard.length !== 13)) {
+        this.$swal.fire(
+          'Your idcard must be 13 characters',
+          'You clicked the button!',
+          'error'
+        )
+      } else {
         const result = await axios.post('http://localhost:8080/api/auth/register', data)
         console.log(result.data)
         if (result.data.status === "errorid") {
@@ -92,16 +101,9 @@ export default {
           await this.$router.push('/login')
         }
       }
-      else {
-        this.$swal.fire(
-          'Please enter all inputs !',
-          'You clicked the button!',
-          'error'
-        )
-      }
-
     }
   },
+
 };
 </script>
   
