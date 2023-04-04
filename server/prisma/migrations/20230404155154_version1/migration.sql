f4683c5d4566c0fca57746acc683931e53ea6cd4
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2023-04-04 22:52:32 +0700

    update: update migration

--- server/prisma/migrations/20230404155154_version1/migration.sql
+++ server/prisma/migrations/20230404155154_version1/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+CREATE TYPE "Role" AS ENUM ('DOCTOR', 'USER', 'ADMIN');
 
 -- CreateEnum
 CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'SUCCESS', 'FAILED');
@@ -9,19 +9,20 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "idCard" TEXT NOT NULL,
-    "first_name" TEXT,
-    "last_name" TEXT,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'USER',
+    "hospitalId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "hospitalId" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "UserInfo" (
+CREATE TABLE "user_info" (
     "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "age" INTEGER,
     "sex" TEXT,
     "weight" DOUBLE PRECISION,
@@ -32,29 +33,21 @@ CREATE TABLE "UserInfo" (
     "address" TEXT,
     "religion" TEXT,
     "description" TEXT,
+    "department" TEXT,
+    "metadata" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT NOT NULL,
 
-    CONSTRAINT "UserInfo_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Image" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "metadata" JSONB,
-
-    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "user_info_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Payment" (
     "id" TEXT NOT NULL,
     "userId" TEXT,
+    "status" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "status" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
 
     CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
 );
@@ -63,9 +56,9 @@ CREATE TABLE "Payment" (
 CREATE TABLE "Treatment" (
     "id" TEXT NOT NULL,
     "amount" DOUBLE PRECISION NOT NULL,
+    "userId" TEXT NOT NULL,
     "description" TEXT,
     "paymentId" TEXT,
-    "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -73,15 +66,15 @@ CREATE TABLE "Treatment" (
 );
 
 -- CreateTable
-CREATE TABLE "MedicinePayment" (
+CREATE TABLE "medicine_payment" (
     "id" TEXT NOT NULL,
     "amount" DOUBLE PRECISION NOT NULL,
+    "medicineId" TEXT,
+    "treatmentId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "medicineId" TEXT NOT NULL,
-    "treatmentId" TEXT,
 
-    CONSTRAINT "MedicinePayment_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "medicine_payment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -89,9 +82,8 @@ CREATE TABLE "Medicine" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Medicine_pkey" PRIMARY KEY ("id")
 );
@@ -113,22 +105,22 @@ CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
 CREATE UNIQUE INDEX "User_idCard_key" ON "User"("idCard");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "UserInfo_userId_key" ON "UserInfo"("userId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Image_userId_key" ON "Image"("userId");
+CREATE UNIQUE INDEX "user_info_userId_key" ON "user_info"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Treatment_paymentId_key" ON "Treatment"("paymentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Medicine_name_key" ON "Medicine"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Hospital_name_key" ON "Hospital"("name");
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_hospitalId_fkey" FOREIGN KEY ("hospitalId") REFERENCES "Hospital"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UserInfo" ADD CONSTRAINT "UserInfo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Image" ADD CONSTRAINT "Image_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "user_info" ADD CONSTRAINT "user_info_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Payment" ADD CONSTRAINT "Payment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -140,7 +132,7 @@ ALTER TABLE "Treatment" ADD CONSTRAINT "Treatment_paymentId_fkey" FOREIGN KEY ("
 ALTER TABLE "Treatment" ADD CONSTRAINT "Treatment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "MedicinePayment" ADD CONSTRAINT "MedicinePayment_medicineId_fkey" FOREIGN KEY ("medicineId") REFERENCES "Medicine"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "medicine_payment" ADD CONSTRAINT "medicine_payment_medicineId_fkey" FOREIGN KEY ("medicineId") REFERENCES "Medicine"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "MedicinePayment" ADD CONSTRAINT "MedicinePayment_treatmentId_fkey" FOREIGN KEY ("treatmentId") REFERENCES "Treatment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "medicine_payment" ADD CONSTRAINT "medicine_payment_treatmentId_fkey" FOREIGN KEY ("treatmentId") REFERENCES "Treatment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
